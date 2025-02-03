3d82a746b941d86495ba6379fc9a68a0b55f497a
Diana Barber aka CodingNannah <codingnannah@gmail.com>
2025-02-03 15:50:16 -0700

    css & js for success and error messages

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const input = document.getElementById("input")
+const input = document.getElementById("userInput")
+const messageArea = document.getElementById("messageArea")
+
+function reverseString(str) {
+    // convert str to array & reverse, to string & compare
+    return str.split("").reverse().join("")
+}
 
 function check() {
+    // clear previous message and class
+    messageArea.textContent = "";
+    messageArea.className = "";
+
     const value = input.value;
-    alert(value);
+    // alert(value);
+    const reverse = reverseString(value);
+    // alert(reverse);
+
+    // check if reverse is same
+    if (value === reverse) {
+        showMessage("success", "Palindrome!")
+    } else {
+        showMessage("error", "Not a Palindrome.")
+    }
+        
+}
+
+function showMessage(type, message) {
+    messageArea.className = type;
+    messageArea.textContent = message;
 }
