1b166a841efefc9b2c6b0b17ca8b340978115a85
Diana Barber aka CodingNannah <codingnannah@gmail.com>
2025-02-03 16:19:16 -0700

    brought const input and const messageArea inside check function

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-const input = document.getElementById("userInput")
-const messageArea = document.getElementById("messageArea")
-
 function reverseString(str) {
     // convert str to array & reverse, to string & compare
     return str.split("").reverse().join("")
 }
 
 function check() {
-    // clear input field
+    // moving definition elements inside the function
+    const input = document.getElementById("userInput");
+    const messageArea = document.getElementById("messageArea");
+
+    // clear input field, previous message, and class
     input.value = "";
-    
-    // clear previous message and class
     messageArea.textContent = "";
     messageArea.className = "";
 
@@ -21,14 +20,14 @@ function check() {
 
     // check if reverse is same
     if (value === reverse) {
-        showMessage("Palindrome!", "success")
+        showMessage("Palindrome!", "success", messageArea);
     } else {
-        showMessage("Not a Palindrome.", "error")
+        showMessage("Not a Palindrome.", "error", messageArea);
     }
         
 }
 
-function showMessage(message, type) {
+function showMessage(message, type, messageArea) {
     messageArea.textContent = message;
     messageArea.className = type;
 }
